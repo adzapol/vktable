c7934f3e179a848edbe45565c1a53c2eeefcaac7
adzapol <adzapol13@gmail.com>
2021-04-07 13:35:33 +0300

    ver. 0.1.1

---
+++ src/actions/users.js
@@ -0,0 +1,29 @@
+export function usersFetchData(url) {
+    return async (dispath) => {
+        dispath({ type: 'ITEMS_IS_LOADING' })
+
+        await fetch(url)
+                .then((response) => {
+                    if(!response.ok) {
+                        throw Error(response.statusText)
+                    }
+                    
+                    dispath({ type: 'ITEMS_IS_LOADING' })
+
+                    return response
+                })
+                .then((response) => response.json())
+                .then((data) => dispath({
+                    type: 'ITEMS_FETCH_DATA_SUCCESS',
+                    users: data
+                }))
+                .catch(() => dispath(usersHasErrored(true)))
+    }
+}
+
+export function usersHasErrored(bool) {
+    return {
+        type: 'ITEMS_HAS_ERRORED',
+        hasErrored: bool
+    }
+}
